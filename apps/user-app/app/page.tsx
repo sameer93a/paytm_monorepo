32030df44c10615c2721ffcee757d9ed17e553cc
Sameer Ahmed <sameer937655@gmail.com>
2024-09-05 13:37:05 +0530

    add prisma

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,6 +1,5 @@
-import Image from "next/image";
-import { Button } from "@repo/ui/button";
-import styles from "./page.module.css";
+import { PrismaClient } from "@repo/db/client";
+const client = new PrismaClient();
 
 export default function Home() {
   return <div className="text-3xl">Hello</div>;
